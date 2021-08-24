73840235dc3d0c6bad525776493dbf5cdf6b2c85
bryson crandall <bryson.crandall@yahoo.com>
2021-08-24 14:23:09 -0700

    front end displays movies and directors

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import fetch from 'node-fetch'
 
 function App() {
+
+  const [movies, setMovies] = useState([])
+  const [directors, setDirectors] = useState([])
+  const [loaded, setLoadied] = useState(false)
+
+  const getMovies = async () => {
+    await fetch('http://localhost:8080/movies')
+      .then((res) => res.json())
+      .then((data) => setMovies(data))
+  }
+
+  const getDirectors = async () => {
+    await fetch('http://localhost:8080/directors')
+      .then((res) => res.json())
+      .then((data) => setDirectors(data))
+  }
+
+  useEffect(() => {
+    getMovies()
+    getDirectors()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading"><h3>IMDB</h3></div>
+      <div className="content">
+        <div className="top-left">
+          {movies.map((movie) => <p>{movie.movie_name}:</p>)}
+        </div>
+        <div className="top-right">
+          {directors.map((d) => <div className="director">
+          <span>{d.first_name} {d.last_name}</span>
+          <span>{}</span>
+          </div>
+          )}
+        </div>
+      </div>
     </div>
+
   );
 }
 
